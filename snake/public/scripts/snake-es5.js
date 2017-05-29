797ee6ab6c572270e39197529a70302a571125f3
Jorge Yau <codenameyau@gmail.com>
2017-05-29 00:06:32 -0400

    Fleshing out some of logic for game code

--- snake/public/scripts/snake-es5.js
+++ snake/public/scripts/snake-es5.js
@@ -9,11 +9,17 @@ var _createClass = (function () { function defineProperties(target, props) { for
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 var SnakeGame = (function () {
-  function SnakeGame(canvasId) {
+  function SnakeGame() {
+    var canvasId = arguments.length <= 0 || arguments[0] === undefined ? 'snake-game' : arguments[0];
+    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
     _classCallCheck(this, SnakeGame);
 
-    this.canvas = document.getElementById('snake-game');
+    this.canvas = document.querySelector(canvasId);
     this.ctx = this.canvas.getContext('2d');
+    this.snake = null;
+    this.food = null;
+    this.score = 0;
   }
 
   /********************************************************************
@@ -28,19 +34,80 @@ var SnakeGame = (function () {
       this.ctx.lineTo(300, 150);
       this.ctx.stroke();
     }
+  }, {
+    key: 'start',
+    value: function start() {
+      this.render();
+    }
+  }, {
+    key: 'resetScore',
+    value: function resetScore() {
+      this.score = 0;
+    }
+  }, {
+    key: 'newGame',
+    value: function newGame() {
+      this.spawnSnake();
+      this.spawnFood();
+      this.resetScore();
+      this.start();
+    }
+  }, {
+    key: 'spawnSnake',
+    value: function spawnSnake() {
+      this.snake = new Snake(1, 1, 5, 1);
+    }
+  }, {
+    key: 'spawnFood',
+    value: function spawnFood() {
+      var emptyCell = this.getEmptyCell();
+    }
+  }, {
+    key: 'getEmptyCell',
+    value: function getEmptyCell() {
+      // By definition an empty square is a non-snake segment occupied cell.
+
+    }
+  }, {
+    key: 'isEmptyCell',
+    value: function isEmptyCell() {}
   }]);
 
   return SnakeGame;
 })();
 
-var Snake = function Snake() {
-  _classCallCheck(this, Snake);
-}
+var Snake = (function () {
+  function Snake(x1, y1, x2, y2) {
+    _classCallCheck(this, Snake);
 
-/********************************************************************
-* MAIN PROGRAM
-*********************************************************************/
-;
+    this.start = [x1, y1, x2, y2];
+    this.segments = [];
+    this.createSegments(x1, y1, x2, y2);
+    this.direction = this.computeDirection(x1, y1, x2, y2);
+  }
 
-var game = new SnakeGame();
-game.render();
+  /********************************************************************
+  * MAIN PROGRAM
+  *********************************************************************/
+
+  _createClass(Snake, [{
+    key: 'createSegments',
+    value: function createSegments(x1, y1, x2, y2) {
+      for (var i = x1; i <= x2; i++) {
+        console.log(i);
+        for (var j = y1; j <= y2; j++) {
+          debugger;
+          this.segments.push([i, j]);
+        }
+      }
+    }
+  }, {
+    key: 'computeDirection',
+    value: function computeDirection(x1, y1, x2, y2) {}
+  }]);
+
+  return Snake;
+})();
+
+var game = new SnakeGame('#snake-game', { width: 40, length: 40 });
+game.newGame();
--- snake/public/scripts/snake.js
+++ snake/public/scripts/snake.js
@@ -5,9 +5,12 @@
 *********************************************************************/
 class SnakeGame {
 
-  constructor(canvasId) {
-    this.canvas = document.getElementById('snake-game');
+  constructor(canvasId='snake-game', options={}) {
+    this.canvas = document.querySelector(canvasId);
     this.ctx = this.canvas.getContext('2d');
+    this.snake = null;
+    this.food = null;
+    this.score = 0;
   }
 
   render() {
@@ -16,19 +19,72 @@ class SnakeGame {
     this.ctx.lineTo(300,150);
     this.ctx.stroke();
   }
+
+  start() {
+    this.render();
+  }
+
+  resetScore() {
+    this.score = 0;
+  }
+
+  newGame() {
+    this.spawnSnake();
+    this.spawnFood();
+    this.resetScore();
+    this.start();
+  }
+
+  spawnSnake() {
+    this.snake = new Snake(1, 1, 5, 1);
+  }
+
+  spawnFood() {
+    const emptyCell = this.getEmptyCell();
+  }
+
+  getEmptyCell() {
+    // By definition an empty square is a non-snake segment occupied cell.
+
+  }
+
+  isEmptyCell() {
+
+  }
+
 }
+
 
 
 /********************************************************************
 * SNAKE CLASS
 *********************************************************************/
 class Snake {
+  constructor(x1, y1, x2, y2) {
+    this.start = [x1, y1, x2, y2];
+    this.segments = [];
+    this.createSegments(x1, y1, x2, y2);
+    this.direction = this.computeDirection(x1, y1, x2, y2);
+  }
 
+  createSegments(x1, y1, x2, y2) {
+    for (let i=x1; i<=x2; i++) {
+      console.log(i);
+      for (let j=y1; j<=y2; j++) {
+        debugger;
+        this.segments.push([i, j]);
+      }
+    }
+  }
+
+  computeDirection(x1, y1, x2, y2) {
+
+  }
 }
 
 
 /********************************************************************
 * MAIN PROGRAM
 *********************************************************************/
-const game = new SnakeGame();
-game.render();
+const game = new SnakeGame('#snake-game', { width: 40, length: 40 });
+game.newGame();
