94621073c8747ccd3f886e836e660f346dc064fb
Jorge Yau <codenameyau@gmail.com>
2017-05-29 13:08:45 -0400

    Implement func and tests for createSegments

--- snake/public/scripts/Snake.js
+++ snake/public/scripts/Snake.js
@@ -1,22 +1,49 @@
 export default class Snake {
 
   constructor (x1, y1, x2, y2, options = {}) {
-    this.start = [x1, y1, x2, y2];
-    this.segments = [];
-    this.createSegments(x1, y1, x2, y2);
-    this.direction = this.computeDirection(x1, y1, x2, y2);
+    this.startingPosition = [x1, y1, x2, y2];
     this.color = options.color || '#8df27c';
+    this.segments = [];
+    this.direction = null;
+    this.createSegments(x1, y1, x2, y2);
   }
 
+  // Segments are created from head first, tail last, meaning that:
+  // - The first element in segments is the head--the last element is the tail.
+  // - (x1, y1) is the head and will be added first.
   createSegments (x1, y1, x2, y2) {
-    for (let i = x1; i <= x2; i++) {
-      for (let j = y1; j <= y2; j++) {
-        this.segments.push([i, j]);
+    if (x1 !== x2 && y1 !== y2) {
+      y1 = y2; // Collapse matrix into a vector.
+    }
+
+    if (x1 < x2) {
+      this.direction = 'left';
+      for (let i = x1; i <= x2; i++) {
+        this.segments.push([i, y1]);
+      }
+    }
+
+    else if (x1 > x2) {
+      this.direction = 'right';
+      for (let i = x1; i >= x2; i--) {
+        this.segments.push([i, y1]);
+      }
+      console.log(this.segments);
+    }
+
+    else if (y1 < y2) {
+      this.direction = 'up';
+      for (let i = y1; i <= y2; i++) {
+        this.segments.push([x1, i]);
+      }
+    }
+
+    else if (y1 > y2) {
+      this.direction = 'down';
+      for (let i = y1; i >= y2; i--) {
+        this.segments.push([x1, i]);
       }
     }
   }
 
-  computeDirection (x1, y1, x2, y2) {
-
-  }
 }
--- snake/test/test-snake.js
+++ snake/test/test-snake.js
@@ -5,7 +5,20 @@ describe('Snake', () => {
   describe('.createSegments()', () => {
     describe('Case 1: Create snake segments left to right', () => {
       let snake = new Snake(0, 0, 2, 0);
-      it('should create the correct segments', () => {
+
+      it('should be facing direction left', () => {
+        assert.strictEqual(snake.direction, 'left');
+      });
+
+      it('should have head at correct position', () => {
+        assert.sameMembers(snake.segments[0], [0, 0]);
+      });
+
+      it('should have tail at correct position', () => {
+        assert.sameMembers(snake.segments[snake.segments.length - 1], [2, 0]);
+      });
+
+      it('should create the correct number of segments', () => {
         assert.sameMembers(snake.segments[0], [0, 0]);
         assert.sameMembers(snake.segments[1], [1, 0]);
         assert.sameMembers(snake.segments[2], [2, 0]);
@@ -14,9 +27,66 @@ describe('Snake', () => {
 
     describe('Case 2: Create snake segments right to left', () => {
       let snake = new Snake(2, 0, 0, 0);
-      it('should create the correct segments', () => {
+
+      it('should be facing direction right', () => {
+        assert.strictEqual(snake.direction, 'right');
+      });
+
+      it('should have head at correct position', () => {
+        assert.sameMembers(snake.segments[0], [2, 0]);
+      });
+
+      it('should have tail at correct position', () => {
+        assert.sameMembers(snake.segments[snake.segments.length - 1], [0, 0]);
+      });
+
+      it('should create the correct number of segments', () => {
         assert.sameMembers(snake.segments[0], [2, 0]);
         assert.sameMembers(snake.segments[1], [1, 0]);
+        assert.sameMembers(snake.segments[2], [0, 0]);
+      });
+    });
+
+    describe('Case 3: Create snake segments up to down', () => {
+      let snake = new Snake(0, 0, 0, 2);
+
+      it('should be facing direction up', () => {
+        assert.strictEqual(snake.direction, 'up');
+      });
+
+      it('should have head at correct position', () => {
+        assert.sameMembers(snake.segments[0], [0, 0]);
+      });
+
+      it('should have tail at correct position', () => {
+        assert.sameMembers(snake.segments[snake.segments.length - 1], [0, 2]);
+      });
+
+      it('should create the correct number of segments', () => {
+        assert.sameMembers(snake.segments[0], [0, 0]);
+        assert.sameMembers(snake.segments[1], [0, 1]);
+        assert.sameMembers(snake.segments[2], [0, 2]);
+      });
+    });
+
+    describe('Case 4: Create snake segments down to up', () => {
+      let snake = new Snake(0, 2, 0, 0);
+
+      it('should be facing direction down', () => {
+        assert.strictEqual(snake.direction, 'down');
+      });
+
+      it('should have head at correct position', () => {
+        assert.sameMembers(snake.segments[0], [2, 0]);
+      });
+
+      it('should have tail at correct position', () => {
+        assert.sameMembers(snake.segments[snake.segments.length - 1], [0, 0]);
+      });
+
+      it('should create the correct number of segments', () => {
+        assert.sameMembers(snake.segments[0], [0, 2]);
+        assert.sameMembers(snake.segments[1], [0, 1]);
         assert.sameMembers(snake.segments[2], [0, 0]);
       });
     });
