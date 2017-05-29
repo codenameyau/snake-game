5711c7a7f12fdbf6c63a8bb3d0769e2091d15e16
Jorge Yau <codenameyau@gmail.com>
2017-05-29 12:25:44 -0400

    First mocha test with segments

--- snake/test/test-snake.js
+++ snake/test/test-snake.js
@@ -2,7 +2,23 @@ import { assert } from 'chai';
 import Snake from '../public/scripts/Snake';
 
 describe('Snake', () => {
+  describe('.createSegments()', () => {
+    describe('Case 1: Create snake segments left to right', () => {
+      let snake = new Snake(0, 0, 2, 0);
+      it('should create the correct segments', () => {
+        assert.sameMembers(snake.segments[0], [0, 0]);
+        assert.sameMembers(snake.segments[1], [1, 0]);
+        assert.sameMembers(snake.segments[2], [2, 0]);
+      });
+    });
 
+    describe('Case 2: Create snake segments right to left', () => {
+      let snake = new Snake(2, 0, 0, 0);
+      it('should create the correct segments', () => {
+        assert.sameMembers(snake.segments[0], [2, 0]);
+        assert.sameMembers(snake.segments[1], [1, 0]);
+        assert.sameMembers(snake.segments[2], [0, 0]);
+      });
+    });
+  });
 });
-
-console.log(Snake);
