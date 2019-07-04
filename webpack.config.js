dd490d04135c080a1988c4a546b65a910f89e833
Jorge Yau <codenameyau@gmail.com>
2019-07-03 23:34:15 -0400

    Update webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,11 @@ module.exports = {
             loader: "html-loader"
           }
         ]
-      }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ]
   },
   plugins: [
